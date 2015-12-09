045311f0edf253e70b836fdfad29e429fff180ca
Bede Constantinides <bedeabc@gmail.com>
2015-12-09 14:51:00 +0000

    Substitution bug fix

--- sod.py
+++ sod.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# Sample and mutate subsequences from a supplied reference nucleotide sequence
+# SoD: sample and mutate subsequences from a supplied reference nucleotide sequence
 # USAGE ./sod.py -n 10000 -l 250 -s 0.1 -i 0.05 -d 0.05 tests/hxb2.fasta > outfile.fa
 
 __author__ = 'Bede Constantinides'
 __license__ = 'GPL v3'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import sys
 import argh
@@ -65,7 +65,6 @@ def simulate(ref_fwd, ref_rev_cmp, ref_len, mut_read_len, ids_masks):
     start_pos = random.randint(0, ref_len-mut_read_len)
     direction = 'fwd' if random.getrandbits(1) else 'rev'
     ref = str(ref_fwd) if direction == 'fwd' else str(ref_rev_cmp)
-
     ref_i = start_pos
     read = ''
     for insertion, deletion, substitution in zip(ins_mask, del_mask, sub_mask):
@@ -74,10 +73,9 @@ def simulate(ref_fwd, ref_rev_cmp, ref_len, mut_read_len, ids_masks):
             read += base + rand_seq(insertion)
         if deletion:
             ref_i += deletion
-        ref_i += 1
-        if not insertion and not substitution:
+        if not insertion:
             read += base
-
+        ref_i += 1
     start_pos = start_pos if direction == 'fwd' else ref_len - start_pos
     end_pos = start_pos + mut_read_len if direction == 'fwd' else start_pos - mut_read_len
     read_header = direction + '_s' + str(start_pos) + '_e' + str(end_pos)
@@ -85,7 +83,7 @@ def simulate(ref_fwd, ref_rev_cmp, ref_len, mut_read_len, ids_masks):
     return record
 
 
-def main(path_to_ref, n_reads=1, len_reads=250, sub_rate=0.05, ins_rate=0.025, del_rate=0.025):
+def main(path_to_ref, n_reads=1, len_reads=250, sub_rate=0.0, ins_rate=0.0, del_rate=0.0):
     ref_fwd = str(SeqIO.read(path_to_ref, 'fasta').seq)
     ref_rev_cmp = Seq(ref_fwd, DNAAlphabet()).reverse_complement()
     ref_len = len(ref_fwd)
@@ -94,6 +92,7 @@ def main(path_to_ref, n_reads=1, len_reads=250, sub_rate=0.05, ins_rate=0.025, d
     for i in range(n_reads):
         ids_masks, mut_read_len = mutate(len_reads, sub_rate, ins_rate, del_rate)
         record = simulate(ref_fwd, ref_rev_cmp, ref_len, mut_read_len, ids_masks)
+        assert len(record.seq) == len_reads
         records.append(record)
         print('>' + record.id)
         print(record.seq)
